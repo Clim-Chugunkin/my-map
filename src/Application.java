da514d2cae4ea18b165d417d0b5dd1aaf9b029da
Kislev Sergey <mr.sergey.kiselev.85@mail.ru>
2025-07-14 15:21:11 +0700

    add test

--- src/Application.java
+++ src/Application.java
@@ -1,11 +1,8 @@
-import java.util.Map;
-import java.util.Objects;
-import java.util.TreeMap;
-import java.util.Comparator;
+
 
 public class Application{
   public static void main(String[] args){
     System.out.println("Goodbay World");
-  
+
   }
 }
